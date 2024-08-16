465540bb12f26027d6774dd6b5c08fc09f9d8928
Arhan Chaudhary <arhan.ch@gmail.com>
2024-08-16 13:30:36 -0700

    test suite in html

--- contentScript.js
+++ contentScript.js
@@ -10,5 +10,6 @@ let script = document.createElement("script");
 script.src = chrome.runtime.getURL("strictEquality.js");
 script.onload = function () {
   this.remove();
+  document.dispatchEvent(new Event("strictEqualityLoaded"));
 };
 (document.head || document.documentElement).appendChild(script);
